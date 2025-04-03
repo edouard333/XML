aa5f532cdea6ec452ea369f6d5fbc54d47d51bef
edouard333 <edouard128@hotmail.com>
2025-04-03 14:18:21 +0200

    Amélioration du code.
    * Amélioration du pom.xml
    * Ajout de checksul lors du build.
    * Mise à jour dépendances.
    * Tri des fonctions dans la classe XML et ajout d'annotation.

--- src/main/java/com/phenix/xml/XML.java
+++ src/main/java/com/phenix/xml/XML.java
@@ -1,8 +1,10 @@
 package com.phenix.xml;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
 import java.util.ArrayList;
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -16,44 +18,51 @@ import org.w3c.dom.NodeList;
 public abstract class XML {
 
     /**
-     * Retourne le node enfant via son nom.
+     * Ajoute un node vide.
      *
-     * @param list Le node où on doit chercher.
-     * @param name String Nom du node à trouver.
-     * @return Retourne le node demandé, sinon retourne {@code null} s'il ne
-     * trouve pas.
+     * @param document Document.
+     * @param name Nom du node.
+     * @return Le node créé.
      */
-    @Null
-    protected static NodeList getNodeListByName(@NotNull NodeList list, String name) {
-        for (int i = 0; i < list.getLength(); i++) {
-            if (list.item(i).getNodeName().equals(name)) {
-                return ((Element) list.item(i)).getChildNodes();
-            }
-        }
-
-        return null;
+    protected static Node addNode(@NotNull Document document, @NotBlank String name) {
+        return addNode(document, document.getDocumentElement(), name);
     }
 
     /**
-     * Retourne une liste de node enfant via son nom.<br>
-     * Donc il existe plusieurs noms enfant portant le même nom.
+     * Ajoute un node vide.
      *
-     * @param node Le node parent.
-     * @param name Nom du node à trouver.
-     * @return Retourne une liste des nodes trouvé.
+     * @param document Document.
+     * @param node_parent Le node parent où ajouter.
+     * @param name Nom du node.
+     * @return Le node créé.
      */
-    @NotNull
-    protected static ArrayList<Node> getChildNodeListByName(@NotNull Node node, String name) {
-        ArrayList<Node> liste_node = new ArrayList<Node>();
-        NodeList list = node.getChildNodes();
+    protected static Node addNode(@NotNull Document document, @NotNull Node node_parent, @NotBlank String name) {
+        return node_parent.appendChild(document.createElement(name));
+    }
 
-        for (int i = 0; i < list.getLength(); i++) {
-            if (list.item(i).getNodeName().equals(name)) {
-                liste_node.add(list.item(i));
-            }
-        }
+    /**
+     * Ajoute un node à la racine avec une valeur.
+     *
+     * @param document Document.
+     * @param name Nom du node.
+     * @param value Valeur du node.
+     */
+    protected static void addNode(@NotNull Document document, @NotBlank String name, String value) {
+        addNode(document, document.getDocumentElement(), name, value);
+    }
 
-        return liste_node;
+    /**
+     * Ajoute un node avec une valeur.
+     *
+     * @param document Document.
+     * @param node_parent Le node parent où ajouter.
+     * @param name Nom du node.
+     * @param value Valeur du node.
+     */
+    protected static void addNode(@NotNull Document document, @NotNull Node node_parent, @NotBlank String name, String value) {
+        Element element = document.createElement(name);
+        element.setTextContent(value);
+        node_parent.appendChild(element);
     }
 
     /**
@@ -63,12 +72,12 @@ public abstract class XML {
      * @param name Nom de l'attribut.
      * @return Valeur de l'attribut.
      */
-    protected static String getAttributValueByName(@NotNull Node node, String name) {
+    protected static String getAttributValueByName(@NotNull Node node, @NotBlank String name) {
         return node.getAttributes().getNamedItem(name).getNodeValue();
     }
 
     /**
-     * Retourne un {@code Node} selon son nom dans un {@code Node} parent.
+     * Retourne un {@link Node} selon son nom dans un {@link Node} parent.
      *
      * @param node Le node où faire la recherche.
      * @param nom_node Le nom du node.
@@ -85,6 +94,28 @@ public abstract class XML {
         }
 
         return null;
+    }
+
+    /**
+     * Retourne une liste de node enfant via son nom.<br>
+     * Donc il existe plusieurs noms enfant portant le même nom.
+     *
+     * @param node Le node parent.
+     * @param name Nom du node à trouver.
+     * @return Liste des nodes trouvé.
+     */
+    @NotNull
+    protected static ArrayList<Node> getChildNodeListByName(@NotNull Node node, String name) {
+        ArrayList<Node> liste_node = new ArrayList<Node>();
+        NodeList list = node.getChildNodes();
+
+        for (int i = 0; i < list.getLength(); i++) {
+            if (list.item(i).getNodeName().equals(name)) {
+                liste_node.add(list.item(i));
+            }
+        }
+
+        return liste_node;
     }
 
     /**
@@ -107,6 +138,24 @@ public abstract class XML {
     }
 
     /**
+     * Retourne le node enfant via son nom.
+     *
+     * @param list Le node où on doit chercher.
+     * @param name String Nom du node à trouver.
+     * @return Le node demandé, sinon retourne {@code null} s'il ne trouve pas.
+     */
+    @Null
+    protected static NodeList getNodeListByName(@NotNull NodeList list, String name) {
+        for (int i = 0; i < list.getLength(); i++) {
+            if (list.item(i).getNodeName().equals(name)) {
+                return ((Element) list.item(i)).getChildNodes();
+            }
+        }
+
+        return null;
+    }
+
+    /**
      * Retourne la valeur (contenu dans le node pas l'attribut 'value') d'un
      * node enfant.
      *
@@ -122,5 +171,19 @@ public abstract class XML {
         } else {
             return null;
         }
+    }
+
+    /**
+     * Modifie la valeur (contenu dans le node pas l'attribut 'value') d'un node
+     * enfant.<br>
+     * Si le champ n'existe pas, on fait planté le système.
+     *
+     * @param node Node parent.
+     * @param name Nom du node enfant.
+     * @param value La valeur.
+     */
+    protected static void setNodeValueByName(@NotNull Node node, String name, String value) {
+        Node child = getNodeByName(node, name);
+        child.setTextContent(value);
     }
 }
